eba8306cfdeb4b08a857291543fffc8f3ea082c9
marvelcn015 <marvelcn015@gmail.com>
2025-02-15 03:39:44 +0800

    chore: initialize spring boot project

---
+++ src/main/java/com/qaron/springbootmallapi/SpringbootMallApiApplication.java
@@ -0,0 +1,13 @@
+package com.qaron.springbootmallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qaron/springbootmallapi/SpringbootMallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qaron.springbootmallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
